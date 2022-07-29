61c0574e156a296be3816fa5a849c021de83c67c
Colm Bhandal <bhandalc@gmail.com>
2022-07-29 09:09:11 +0100

    Fixed typo

--- types/userAddressData.ts
+++ types/userAddressData.ts
@@ -7,7 +7,7 @@ export type UserAddressData = {
     address: string
 }
 
-export function isUseAddressrData(obj: any): obj is UserAddressData{
+export function isUseAddressData(obj: any): obj is UserAddressData{
     if(!obj){
         return false
     }
