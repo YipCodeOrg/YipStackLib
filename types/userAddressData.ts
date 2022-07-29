af0f15736c96a6afc5aeb837ddcdafec23e37cd2
Colm Bhandal <bhandalc@gmail.com>
2022-07-29 11:36:51 +0100

    Chekc User address data array type
    Lifting type predicate up to array level

--- types/userAddressData.ts
+++ types/userAddressData.ts
@@ -1,10 +1,14 @@
-import { areSimpleStringProperties } from "../util/typePredicates"
+import { areSimpleStringProperties, isTypedArray } from "../util/typePredicates"
 
 export type UserAddressData = {
     sub: string,
     yipCode: string,
     //TODO: Change address to structured type
     address: string
+}
+
+export function isUserAddressDataArray(obj: any): obj is UserAddressData[]{
+    return isTypedArray(obj, isUserAddressData)
 }
 
 export function isUserAddressData(obj: any): obj is UserAddressData{
