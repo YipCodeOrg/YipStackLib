0399501be9a79b69946b95882fa0cfdb9e81e8c6
Colm Bhandal <bhandalc@gmail.com>
2022-08-23 21:34:17 +0100

    Is Registration Up to Date

--- types/userAddressData.ts
+++ types/userAddressData.ts
@@ -24,6 +24,10 @@ export type Registration = {
     addressLastUpdated: Date
 }
 
+export function isRegistrationUpToDate(registration: Registration, date: Date){
+    return registration.addressLastUpdated > date
+}
+
 export function isUserAddressDataArray(obj: any): obj is UserAddressData[]{
     return isTypedArray(obj, isUserAddressData)
 }
